89282ad2c22f6be33b603258f1e5044e62f011c3
duckman-97 <72430680+duckman-97@users.noreply.github.com>
2022-09-21 17:27:29 +0900

    09-21-LST

--- todolist2/src/MyList2.js
+++ todolist2/src/MyList2.js
@@ -1,21 +1,40 @@
 import React,{useState} from 'react';
 
 function MyList2 (){
-    const [text,setText] =useState("");
+    
     //const inputChanged = (event) =>{
       //  setText(event.target.value);
     //}
+
+    const [user, setUser] = userState({
+        firstName:'',
+        lastName:'',
+        email:''
+    });
+
+    const inputChanged = (event) =>{
+        setUser({...user,[event.target.name]: event.target.value});
+
+    }
     const handleSubmit = (event)=>{
         alert(`You typed: ${text}`);
+
         event.preventDefault();
         
     }
     
     return (
         <form onSubmit = {handleSubmit}>
-            <input type = "text" onChange = {event => setText(event.target.value)}
-            value = {text}/>
+            <label>FirstName</label>
+            <input type = "text" name = 'firstName' onChange ={inputChanged}></input>
+           <label>lastName</label>
+
+            <input type = 'text' name = 'lastName' onChange = {inputChanged} value = {user.lastname}></input>
             <input type = "submit" value = "Press me"/>
+            <label>Email</label>
+            <input type = 'text' name = 'email' onChange={inputChanged} value = {user.email}/>
+            <input type = 'submit' value = 'Press me'/>
+
         </form>
     );
 };
